e76fa1b981a21a2d96a8036423b869722507bcbf
hvad <david.hannequin@gmail.com>
2020-12-31 19:36:40 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# golang-project-4
+Projet en Golang de monitoring 
+
+Un script qui vérifie l'usage des disques d'un serveur Linux.   
+
+## Pour construire le binaire :
+    # go build main.go -o go-monitoring
+
+## Usage :
+
+    $./go-monitoring -f sample.golang-monitor.yml  
---
+++ main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io/ioutil"
+
+	"github.com/shirou/gopsutil/disk"
+	"gopkg.in/yaml.v2"
+)
+
+type Config struct {
+	Hostname string
+	Address  string
+	Disks    []string
+}
+
+func check(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func main() {
+
+	var config Config
+
+	var warning int = 80
+	var critical int = 90
+
+	DefaultConfigfile := "/etc/golang-monitor.yml"
+	ConfigFile := flag.String("f", DefaultConfigfile, fmt.Sprintf("Configig file path, default = %s", DefaultConfigfile))
+	flag.Parse()
+
+	CFG, err := ioutil.ReadFile(*ConfigFile)
+	check(err)
+
+	err = yaml.Unmarshal(CFG, &config)
+	check(err)
+
+	DisplayAddr := config.Address
+	DisplayHostname := config.Hostname
+	DisplayDisks := config.Disks
+
+	fmt.Printf("Address : %v\n", DisplayAddr)
+	fmt.Printf("Hostname : %v\n", DisplayHostname)
+
+	for _, d := range DisplayDisks {
+		u, err := disk.Usage(d)
+		check(err)
+		var DisplayUsage int = int(u.UsedPercent)
+		if DisplayUsage >= critical {
+			fmt.Printf("Critical - Disk %v percent usage %v %%\n", u.Path, DisplayUsage)
+
+		} else if DisplayUsage >= warning {
+			fmt.Printf("Warning - Disk %v percent usage %v %%\n", u.Path, DisplayUsage)
+
+		} else {
+
+			fmt.Printf("Ok - Disk %v percent usage %v %%\n", u.Path, DisplayUsage)
+
+		}
+
+	}
+
+}
